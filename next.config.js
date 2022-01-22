a5d8b30db6fbdb316f6ec63471af5bc7fba5fa21
Ashel Joseph Vasquez <vasquezpalominoashel@gmial.com>
2022-01-22 06:53:13 -0500

    Building app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/ashel-root/',
     domains: ['res.cloudinary.com']
   }
 }
