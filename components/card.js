ef2d11c7f00c1e12aa2f77520b167888238cb129
Ashel Joseph Vasquez <vasquezpalominoashel@gmial.com>
2022-01-22 07:51:26 -0500

    Fixed Images v2

--- components/card.js
+++ components/card.js
@@ -12,8 +12,6 @@ export default function Card({...props}) {
                   props.categoria === 'programacion' && 'bg-red-300' ||
                   props.categoria === 'economia' && 'bg-light-blue-200'
 
-  const fixedImageUri = props.image.replace('https://res.cloudinary.com/ashel-root/', '')
-
   return (
     <div className="p-5 bg-white rounded-3xl mb-4 h-full border-2 border-black-200">
       <div className="flex flex-col h-full justify-between">
@@ -22,7 +20,7 @@ export default function Card({...props}) {
             <div className="w-14 h-14 flex-shrink-0 mr-5">
               <Image
                 className="rounded-full"
-                src={fixedImageUri}
+                src={props.image.replace('https://res.cloudinary.com/ashel-root/', '')}
                 alt={props.name}
                 width={56}
                 height={56}
